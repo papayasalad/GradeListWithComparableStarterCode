8b585012dcc768facabc6b7dd64c39eef8924942
papayasalad <papayasaladgo@gmail.com>
2019-04-16 16:46:04 -0700

    revise all necessary methods of grades

--- src/main/java/Grade.java
+++ src/main/java/Grade.java
@@ -1,36 +1,44 @@
-public class Grade {
+public class Grade implements Comparable<Grade> {
     private String student;
     private double grade;
 
     Grade(String student, double grade) {
-        // TODO constructor that sets the student and grade fields
+        this.student = student;
+        this.grade = grade;
     }
 
     public String toLetterGrade() {
-        // TODO return the letter grade corresponding to the grade field
         // >= 90 is a A, 80-89 B, 70-79 C, 60-69 D, < 60 F
-        return null;
+        if (this.grade >= 90) {
+            return "A";
+        } else if (this.grade >= 80) {
+            return "B";
+        } else if (this.grade >= 70) {
+            return "C";
+        } else if (this.grade >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
     }
 
     public String getStudent() {
-        // TODO getter for the student field
-        return null;
+        return this.student;
     }
 
     public double getGrade() {
-        // TODO getter for the grade field
-        return 0.0;
+        return this.grade;
     }
 
     public String toString() {
-        // TODO return a useful representation of the object, including the name and grade fields
-        return null;
+        StringBuilder res = new StringBuilder();
+        res.append(this.student + ":" + this.grade);
+        return res.toString();
     }
 
     public int compareTo(Grade other) {
-        // TODO implement compareTo.
         // Don't forget anything else that might be missing in this class for this function to be called correctly.
-        return 0;
+        return Double.compare(this.grade, other.grade);
     }
 
 }
--- src/main/java/Grades.java
+++ src/main/java/Grades.java
@@ -3,68 +3,103 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Grades {
-    private List<Double> grades;
+    private List<Grade> grades;
 
     public Grades() {
         grades = new ArrayList<>();
     }
 
-    public List<Double> getGrades() {
+    public List<Grade> getGrades() {
         return grades;
     }
 
     public void readGrades(String fileName) throws FileNotFoundException {
         Scanner scan = new Scanner(new File(fileName));
-        while (scan.hasNextLine()) {
-            grades.add(Double.parseDouble(scan.nextLine()));
+        while (scan.hasNext()) {
+            String studentName = scan.next();
+            studentName = studentName.substring(0,studentName.length() - 1);
+            double studentGrade = (Double.parseDouble(scan.next()));
+
+            Grade grade = new Grade(studentName, studentGrade);
+            grades.add(grade);
         }
     }
 
     public double calcAverage() {
         double total = 0.0;
-        for (double grade : grades) {
-            total += grade;
+        for (Grade grade : grades) {
+            total += grade.getGrade();
         }
         return total / grades.size();
     }
 
-    public double dropLowest() {
-        double minGrade = Double.MAX_VALUE;
-        for (double grade : grades) {
-            if (grade < minGrade) {
-                minGrade = grade;
+    public Double dropLowest() {
+        Grade lowest = grades.get(0);
+        double minGrade = grades.get(0).getGrade();
+        for (Grade grade : grades) {
+            if (grade.getGrade() < minGrade) {
+                lowest = grade;
             }
         }
-        grades.remove(minGrade);
-        return minGrade;
+        grades.remove(lowest);
+        return lowest.getGrade();
     }
 
-    public void addGrade(double grade) {
+    public void addGrade(Grade grade) {
         grades.add(grade);
     }
 
+    //return grades.removeAll(Collections.singleton(grade));
     public boolean removeAllGrades(double grade) {
-        return grades.removeAll(Collections.singleton(grade));
+        boolean found = false;
+        for (int i = 0; i < grades.size(); i++) {
+            if (grades.get(i).getGrade() == grade) {
+                grades.remove(i);
+                i--;
+                found = true;
+            }
+        }
+        return found;
     }
 
     // Collections.sort sorts the input list as a side-effect, which I didn't prefer
     // So I created a copy and sorted that.
     // You could also just sort grades.
     public void printSortedGrades() {
-        List<Double> gradesCopy = new ArrayList<>(grades);
+        List<Grade> gradesCopy = new ArrayList<>(grades);
         Collections.sort(gradesCopy);
-
         System.out.println(gradesCopy);
     }
 
     public void printGradeBreakdown() {
-        // TODO print the number of students who got A, B, C, D, and F
         // e.g. A: 7, B: 5, C: 2, D: 1, F: 1
+        int[] cnt = new int[5];
+        for (int i = 0; i < grades.size(); i++) {
+            String letter = grades.get(i).toLetterGrade();
+            if (letter.equalsIgnoreCase("A")) {
+                cnt[0]++;
+            } else if (letter.equalsIgnoreCase("B")) {
+                cnt[1]++;
+            } else if (letter.equalsIgnoreCase("C")) {
+                cnt[2]++;
+            } else if (letter.equalsIgnoreCase("D")) {
+                cnt[3]++;
+            } else {
+                cnt[4]++;
+            }
+        }
+        System.out.printf("A: %d, B: %d, C: %d, D: %d, F: %d", cnt[0], cnt[1], cnt[2], cnt[3], cnt[4]);
     }
 
     public String getStudentWithHighestGrade() {
-        // TODO return the student who got the highest grade
-        return null;
+        String res = grades.get(0).getStudent();
+        Double highestGrade = grades.get(0).getGrade();
+        for (Grade grade : grades) {
+            if (grade.getGrade() > highestGrade) {
+                res = grade.getStudent();
+            }
+        }
+        return res;
     }
 
     // Simplest version of toString() for this method
@@ -76,10 +111,10 @@ public class Grades {
     public String toString() {
         StringBuilder result = new StringBuilder("[");
         for (int i = 0; i < grades.size() - 1; i++) {
-            result.append(grades.get(i));
+            result.append(grades.get(i).getStudent() + ":" + grades.get(i).getGrade());
             result.append(", ");
         }
-        result.append(grades.get(grades.size() - 1));
+        result.append(grades.get(grades.size() - 1).getStudent() + ":" + grades.get(grades.size() - 1).getGrade());
         result.append("]");
 
         return result.toString();
